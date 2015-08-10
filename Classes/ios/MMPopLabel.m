e674015d8fb4e659ae1c62185a23fe6a3c61f077
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-08-10 12:45:38 -0400

    checks whether delegate responds to optional protocol selectors before calling them

--- Classes/ios/MMPopLabel.m
+++ Classes/ios/MMPopLabel.m
@@ -306,7 +306,7 @@ typedef enum : NSUInteger {
         self.alpha = 0.0f;
     } completion:^(BOOL finished) {
         self.hidden = YES;
-        [_delegate dismissedPopLabel:self];
+        if([_delegate respondsToSelector:@selector(dismissedPopLabel:)]) [_delegate dismissedPopLabel:self];
     }];
 }
 
@@ -350,7 +350,7 @@ typedef enum : NSUInteger {
 - (void)buttonPressed:(id)sender
 {
     UIButton *button = (UIButton *)sender;
-    if (_delegate != nil) {
+    if ([_delegate respondsToSelector:@selector(didPressButtonForPopLabel:atIndex:)]) {
         [_delegate didPressButtonForPopLabel:self atIndex:button.tag];
     }
     [self dismiss];
